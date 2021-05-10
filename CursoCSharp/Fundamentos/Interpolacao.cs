7eeb3ea01a59ffc2fa877bc352283b21c771832f
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-09 22:19:16 -0300

    Interpolação

--- CursoCSharp/Fundamentos/Interpolacao.cs
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -17,6 +17,7 @@ namespace CursoCSharp.Fundamentos
             Console.WriteLine("O " + nome + " da marca " + marca + " custa " + preco + ".");
             Console.WriteLine("O {0} da marca {1} custa {2}.", nome, marca, preco);
             Console.WriteLine($"O {nome} da marca {marca} custa {preco}.");
+            Console.WriteLine($"1 + 1 = {1 + 1}!");
         }
     }
 }
