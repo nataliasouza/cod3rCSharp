7adee0fddeae79e8fef30ccc843321c64cc0a06c
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-09 21:55:26 -0300

    Curso C# - Fundamentos

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }
+    }
+}
---
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            // area da circunferencia
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+            // PI = 3.1415
+
+            double area = PI * raio * raio;
+            Console.WriteLine(area);
+            Console.WriteLine("Área é " + (area + 1000));
+
+            // Tipos internos
+
+            bool estaChovendo = true;
+            Console.WriteLine("Está chovendo " + estaChovendo);
+
+            byte idade = 45;
+            Console.WriteLine("Idade " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de Gols " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salário " + salario);
+
+            int menorValorInt = int.MinValue;
+            Console.WriteLine("Menor valor do int " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("População Brasileira " + populacaoBrasileira);
+
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor valor do Long " + menorValorLong);
+
+            ulong populacaoMundial = 7_800_900_120;
+            Console.WriteLine("População Mundial " + populacaoMundial);
+
+            float precoComputador = 5999.99f;
+            Console.WriteLine("Preço do Computador " + precoComputador);
+
+            double valorDeMercadoApple = 1_000_000_000_000.00;
+            Console.WriteLine("Valor de Mercado da Apple " + valorDeMercadoApple);
+
+            decimal distanciaEntreEstrelas = decimal.MaxValue;
+            Console.WriteLine("Distância entre Estrelas " + distanciaEntreEstrelas);
+
+            char letra = 'b';
+            Console.WriteLine("Letra " + letra);
+
+            string texto = "Seja bem vindo ao Curso de C#!";
+            Console.WriteLine(texto);
+
+        }
+    }
+}
