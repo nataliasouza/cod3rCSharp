4878e1fb717f7c9f6faba7af83e96ae395958ffd
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-09 22:40:56 -0300

    Notação Ponto

--- CursoCSharp/Fundamentos/NotacaoPonto.cs
+++ CursoCSharp/Fundamentos/NotacaoPonto.cs
@@ -10,7 +10,7 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
-            var saudacao = "olá".ToUpper().Insert(3, "World!").Replace("World", "Mundo!");
+            var saudacao = "olá".ToUpper().Insert(3, " World!").Replace("World", "Mundo!");
             Console.WriteLine(saudacao);
 
             Console.WriteLine("Teste".Length);
