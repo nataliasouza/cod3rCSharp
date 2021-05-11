3cae474c3b986ad071ef41a47462a3ac202c4280
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-10 21:30:24 -0300

    CursoCSharp

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -14,6 +14,7 @@ namespace CursoCSharp {
                 {"Interpolacao - Fundamentos", Interpolacao.Executar},
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
                 {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
             });
 
             central.SelecionarEExecutar();
