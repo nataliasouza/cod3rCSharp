52d9572bd168964c26134a4c0eaa85b90f8b1bcf
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-11 22:09:16 -0300

    OperadorTernario

---
+++ CursoCSharp/Fundamentos/OperadorTernario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadorTernario
+    {
+        public static void Executar()
+        {
+            var nota = 7.0;
+            bool bomComportamento = false;
+            //var resultado = nota >= 7.0 ? "Aprovado" : "Reprovado";
+            var resultado = nota >= 7.0 && bomComportamento ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
+}
