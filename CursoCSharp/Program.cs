2d716b11826bf08da7c52bd7516c59230392179f
nataliasouza <nataliasouza.ti@hotmail.com>
2021-05-11 22:47:46 -0300

    EstruturaIf

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                
+                //FUNDAMENTOS
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
@@ -21,6 +24,10 @@ namespace CursoCSharp {
                 {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
                 {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+                
+                // ESTRUTURA DE CONTROLE
+                {"Estrutura IF - Estrutura de Controle", OperadorTernario.Executar},
+
             });
 
             central.SelecionarEExecutar();
