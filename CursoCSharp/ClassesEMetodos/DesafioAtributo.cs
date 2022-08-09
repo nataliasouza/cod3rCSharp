6012955108b548559cf1c9b82018d1500715547e
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-08-09 12:54:39 -0300

    Desafio Acessar Atributo

---
+++ CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class DesafioAtributo
+    {
+        int a = 10;
+        public static void Executar()
+        {
+            DesafioAtributo desafio = new DesafioAtributo();         
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
