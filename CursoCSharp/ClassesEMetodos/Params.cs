a98b201229cad59afbe5ff9b28de2040a112c790
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-08-09 12:55:15 -0300

    Params

---
+++ CursoCSharp/ClassesEMetodos/Params.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Params
+    {
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine("Olá {0}", pessoa);
+            }
+        }
+        public static void Executar()
+        {
+            Recepcionar("Nat", "Max", "Teddy");
+        }
+    }
+}
